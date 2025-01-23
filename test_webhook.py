251794c19e7a3fd64e00032767bd4e3f4ea2b1f4
Mariam Eissa <mariamkhaled999@gmail.com>
2025-01-23 13:01:44 +0200

    Update test_webhook.py

--- test_webhook.py
+++ test_webhook.py
@@ -6,8 +6,8 @@ def say_hello_test_webhook77888():
   return "say_hello_test_webhook7"
 
 
-def say_hello_test_webhook66():
-  return "say_hello_test_webhook66"
+def say_hello_test_webhook666():
+  return "say_hello_test_webhook666"
 
 
 
